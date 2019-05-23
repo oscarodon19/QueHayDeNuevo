4b6dedb71ddaa85c04445d364c77598309406104
oscar odon <oscarodon19@gmail.com>
2019-05-22 21:48:44 -0300

    first commit

---
+++ Que hay de nuevo/Scene.swift
@@ -0,0 +1,26 @@
+//
+//  Scene.swift
+//  Que hay de nuevo
+//
+//  Created by Oscar Odon on 22/05/2019.
+//  Copyright © 2019 Oscar Odon. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+    }
+}
